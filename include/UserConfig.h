fbfba596542fea16ceca1c992584c4c4033c04d1
Evan Newman <evannewman@gatech.edu>
2020-02-26 11:58:35 -0500

    Added forcing scheme

--- include/UserConfig.h
+++ include/UserConfig.h
@@ -2,9 +2,9 @@
 #define USER_CONFIG_H
 
 /* Control Parameters */
-#define NUM_TIMESTEPS    (1000)
-#define OUTPUT_INTERVAL  (10)
-#define OUTPUT_START     (1)
+#define NUM_TIMESTEPS    (500000)
+#define OUTPUT_INTERVAL  (500)
+#define OUTPUT_START     (50000)
 
 /* Doman Parameters */
 #define SIZE_X    (512)
@@ -15,19 +15,34 @@
 #define D2_Q9
 //#define D3_Q15
 
+/* Relaxation Scheme */
+#define SINGLE_RELAXATION
+
 /* Precision - default is single precision*/
 //#define DOUBLE_PRECISION
 
-// STL Units per lattice spacing
-#define STL_TO_LATTICE (1.0)
-
-// Initial Parameters
-#define INIT_U_X   (0.01)
+/* Initial Velocity */
+#define INIT_U_X   (0.0)
 #define INIT_U_Y   (0.0)
 #define INIT_U_Z   (0.0)
 
+/* Initial Density */
 #define INIT_RHO   (1.0)
 
+/* Kinematic Viscosity */
 #define VISCOSITY  (0.001)
+
+/* Constant Force */
+#define ENABLE_FORCE
+#define FORCE_X    (0.00000002)
+#define FORCE_Y    (0.0)
+#define FORCE_Z    (0.0)
+
+/* Cylinder Boundary */
+#define ENABLE_CYLINDER
+#define CYLINDER_X0   (128)
+#define CYLINDER_Y0   (256)
+#define CYLINDER_R   (5)
+
 
 #endif
--- src/Kernels.cu
+++ src/Kernels.cu
@@ -8,20 +8,23 @@ __global__ void lbmKernel(LatticeParams* params, ObservableArrays* arrays,
 	int z = blockIdx.y;
 	int index = I3D(x, y, z, SIZE_X, SIZE_Y);
 
-	real f[NUM_DIRECTIONS];
+	if (nodeType[index] == 0) {
 
-	/* --- Streaming: pull from 0 to 1 --- */
-	// i = 0 case is easy
-	f[0] = fOld[index*NUM_DIRECTIONS];
-	// loop through remaining distributions
-	for (int i = 1; i < NUM_DIRECTIONS; i++) {
-		// default periodic boundary in each direction
-		int indexei = I3D(((x - int(params->ei[i].x) + SIZE_X)%SIZE_X),
-						  ((y - int(params->ei[i].y) + SIZE_Y)%SIZE_Y),
-						  ((z - int(params->ei[i].z) + SIZE_Z)%SIZE_Z),
-						  SIZE_X, SIZE_Y);
+		real f[NUM_DIRECTIONS];
 
-		if (nodeType[index] == 0) {
+		/* --- Streaming: pull from 0 to 1 --- */
+		// i = 0 case is easy
+		f[0] = fOld[index*NUM_DIRECTIONS];
+		// loop through remaining distributions
+
+		for (int i = 1; i < NUM_DIRECTIONS; i++) {
+			// default periodic boundary in each direction
+			int indexei = I3D(((x - int(params->ei[i].x) + SIZE_X)%SIZE_X),
+							  ((y - int(params->ei[i].y) + SIZE_Y)%SIZE_Y),
+							  ((z - int(params->ei[i].z) + SIZE_Z)%SIZE_Z),
+							  SIZE_X, SIZE_Y);
+
+
 			switch (nodeType[indexei]) {
 			case 0:
 				f[i] = fOld[I3Df(indexei, i, NUM_DIRECTIONS)];
@@ -33,30 +36,75 @@ __global__ void lbmKernel(LatticeParams* params, ObservableArrays* arrays,
 				f[i] = fOld[I3Df(index, i, NUM_DIRECTIONS)];
 			}
 
-		} else {
-			f[i] = fOld[I3Df(index, i, NUM_DIRECTIONS)];
 		}
-	}
 
-	real rho = 0.0;
-	for (int i = 0; i < NUM_DIRECTIONS; i++) {
-		rho = rho + f[i];
-	}
-	arrays->rho[index] = rho;
 
-	vect u = {0.0, 0.0, 0.0};
-	for (int i = 0; i < NUM_DIRECTIONS; i++) {
-		u.x = u.x + (f[i]*params->ei[i].x)/rho;
-		u.y = u.y + (f[i]*params->ei[i].y)/rho;
-		u.z = u.z + (f[i]*params->ei[i].z)/rho;
-	}
-	arrays->u[index] = u;
+		real rho = 0.0;
+		for (int i = 0; i < NUM_DIRECTIONS; i++) {
+			rho = rho + f[i];
+		}
 
-	for (int i = 0; i < NUM_DIRECTIONS; i++) {
-		real equilibrium = fEQ(params->wi[i], rho,
-							   params->ei[i], u);
-		fNew[I3Df(index, i, NUM_DIRECTIONS)] = f[i] - (f[i] - equilibrium)/TAU;
-		// fNew[I3Df(index, i, NUM_DIRECTIONS)] = f[i];
+		vect u = {0.0, 0.0, 0.0};
+		for (int i = 0; i < NUM_DIRECTIONS; i++) {
+			u.x = u.x + (f[i]*params->ei[i].x)/rho;
+			u.y = u.y + (f[i]*params->ei[i].y)/rho;
+			u.z = u.z + (f[i]*params->ei[i].z)/rho;
+		}
+
+		#ifdef ENABLE_FORCE
+		const vect force = {FORCE_X, FORCE_Y, FORCE_Z};
+		real* fp = (real*)(&force);
+		real* up = (real*)(&u);
+
+		real source[NUM_DIRECTIONS];
+		for (int i = 0; i < NUM_DIRECTIONS; i++) {
+			real* cip = (real*)(&(params->ei[i]));
+			source[i] = 0.0;
+			for (int a = 0; a < 3; a++) {
+				for (int b = 0; b < 3; b++) {
+					source[i] = source[i] + 
+					fp[a]*(3.0*cip[a] + 9.0*up[b]*(cip[a]*cip[b] - (a == b ? 1.0 : 0.0)/3.0));
+				}
+			}
+			source[i] = source[i]*params->wi[i];
+
+			rho = rho + source[i]/2;
+
+			real calpha = 0.0;
+			for (int a = 0; a < 3; a++) {
+				calpha = calpha + cip[a];
+			}
+
+			u.x = u.x + source[i]*calpha*0.5/rho;
+			u.y = u.y + source[i]*calpha*0.5/rho;
+			u.z = u.z + source[i]*calpha*0.5/rho;
+		}
+
+		#define TAU_BAR  (TAU + 0.5)
+		#else
+		#define TAU_BAR  (TAU)
+		#endif
+
+		arrays->rho[index] = rho;
+		arrays->u[index] = u;
+
+		#ifdef SINGLE_RELAXATION
+		for (int i = 0; i < NUM_DIRECTIONS; i++) {
+			real equilibrium = fEQ(params->wi[i], rho,
+								   params->ei[i], u);
+			f[i] = f[i] - (f[i] - equilibrium)/TAU_BAR;
+		}
+		#endif
+
+		#ifdef ENABLE_FORCE
+		for (int i = 0; i < NUM_DIRECTIONS; i++) {
+			f[i] = f[i] + (1 - 1/(2*TAU_BAR))*source[i];
+		}
+		#endif
+
+		for (int i = 0; i < NUM_DIRECTIONS; i++) {
+			fNew[I3Df(index, i, NUM_DIRECTIONS)] = f[i];
+		}
 	}
 }
 
@@ -72,7 +120,17 @@ __global__ void initKernel(LatticeParams* params, ObservableArrays* arrays,
 	// intialize boundary
 	if (y == 0 || y == SIZE_Y - 1) {
 		nodeType[index] = 1;
-	} else {
+	}
+
+	#ifdef ENABLE_CYLINDER
+	else if ((x - CYLINDER_X0)*(x - CYLINDER_X0) +
+			 (y - CYLINDER_Y0)*(y - CYLINDER_Y0) 
+			  < CYLINDER_R*CYLINDER_R) { 
+		nodeType[index] = 1;
+	}
+	#endif
+
+	else {
 		nodeType[index] = 0;
 	}
 
